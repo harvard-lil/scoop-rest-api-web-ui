a0e3c89c8b525e2253ded51cd04763e94e027dcc
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2023-07-07 18:10:54 -0400

    Scaffolding

---
+++ functions/poll.js
@@ -0,0 +1,11 @@
+exports.handler = async function (event, context) {
+  // Check pin
+  // Check id_capture
+
+  // Proxy request
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World"}),
+  }
+}
